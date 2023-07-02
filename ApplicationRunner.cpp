90c7f85c2ca17b3ca41ab17406f65033071eaae7
tagoras <pvaitkeviciuz@gmail.com>
2023-07-02 16:00:30 +0100

    Added logic to run application

---
+++ ApplicationRunner.cpp
@@ -0,0 +1,29 @@
+#include "ApplicationRunner.h"
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+void Application_Runner::run() const
+{
+    while(true)
+    {
+        std::cout << "> ";
+
+        std::string user_input;
+        getline(std::cin, user_input);
+
+        std::pair<std::string, std::vector<std::string>> pair;
+        pair = command_parser.parseCommand(user_input);
+
+        std::cout << "Executing command: " << pair.first << std::endl;
+        std::cout << "Arguments are:";
+
+        for(std::string arg : pair.second)
+        {
+            std::cout << arg << " ";
+        }
+        std::cout << std::endl;
+    }
+}
