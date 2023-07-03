f2c2b02f445311771aa5fc43fbe20948d9e8b826
tagoras <pvaitkeviciuz@gmail.com>
2023-07-03 10:40:27 +0100

    Removed command manager and substitured with command_picker

--- headers/ApplicationRunner.h
+++ headers/ApplicationRunner.h
@@ -2,10 +2,12 @@
 #define APPLICATION_RUNNER
 
 #include "CommandParser.h"
+#include "CommandPicker.h"
 
 class Application_Runner{
     private:
-        CommandParser command_parser;
+        CommandParser command_parser; 
+        CommandPicker command_picker;
     public:
         Application_Runner() = default;
         void run() const;
