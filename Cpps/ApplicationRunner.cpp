b27accd31a170051a7f6d3da8becae9478c97f15
tagoras <pvaitkeviciuz@gmail.com>
2023-07-03 13:11:45 +0100

    Styling changes

--- Cpps/ApplicationRunner.cpp
+++ Cpps/ApplicationRunner.cpp
@@ -10,17 +10,18 @@ void Application_Runner::run() const
 {
     while(true)
     {
-        std::cout << "> ";
+        std::cout << ":~$ ";
 
         std::string user_input;
         getline(std::cin, user_input);
 
         std::pair<std::string, std::vector<std::string>> pair;
         pair = command_parser.parseCommand(user_input);
-
+        
         std::shared_ptr<ICommand> command_to_execute = command_picker.pick_command(pair.first);
         int command_result = command_to_execute->executeCommand(pair.second);
 
         if(command_result) std::cout << "An error has occured while executing " << pair.first << std::endl;
+        else std::cout << std::endl;
     }
 }
