2f417a2d5022c720717615c73221249b89103577
tagoras <pvaitkeviciuz@gmail.com>
2023-07-03 10:41:08 +0100

    Added command picker header

---
+++ headers/CommandPicker.h
@@ -0,0 +1,23 @@
+#ifndef COMMAND_PICKER
+#define COMMAND_PICKER
+
+#include <string>
+#include <memory>
+
+#include "CommandManager.h"
+#include "ICommand.h"
+
+class CommandPicker{
+    private:
+        Command_Manager command_manager;
+    public:
+        CommandPicker()=default;
+        std::shared_ptr<ICommand> pick_command(const std::string &name) const;
+};
+
+std::shared_ptr<ICommand> CommandPicker::pick_command(const std::string &name) const
+{
+    return command_manager.commands.at(name);
+}
+
+#endif
