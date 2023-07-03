fc86c1e212cc67c597ef12e640fb99396310d136
tagoras <pvaitkeviciuz@gmail.com>
2023-07-03 10:40:55 +0100

    Modified command manager to store commands in a map

--- headers/CommandManager.h
+++ headers/CommandManager.h
@@ -3,15 +3,23 @@
 
 #include <vector>
 #include <string>
+#include <map>
+#include <memory>
 
 #include "ICommand.h"
+#include "Echo.h"
 
 // Class Used to keep all of the shell commands
 class Command_Manager{
-    private:
-        std::vector<ICommand> commands;
+    public:
+        std::map<std::string,std::shared_ptr<ICommand>> commands;
     public:
         Command_Manager();
 };
+
+Command_Manager::Command_Manager()
+{
+    commands.insert(std::pair<std::string, std::shared_ptr<ICommand>>("echo", std::make_shared<Echo>()));
+}
 
 #endif
