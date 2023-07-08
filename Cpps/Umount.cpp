dca900593cf04b57591389e6564467ef650030e3
tagoras <pvaitkeviciuz@gmail.com>
2023-07-08 16:17:05 +0100

    Added umount command

--- Cpps/Umount.cpp
+++ Cpps/Umount.cpp
@@ -3,5 +3,5 @@
 #include <sys/mount.h>
 
 int Umount::executeCommand(const std::vector<std::string> &args) const {
-    return umount2(args[0].c_str(), MNT_DETACH);
+    return umount(args[0].c_str());
 }
