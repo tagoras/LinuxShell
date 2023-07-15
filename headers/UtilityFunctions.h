6f327dfaec90f0c622ba9a5f4d976a315ebef1db
tagoras <pvaitkeviciuz@gmail.com>
2023-07-15 11:56:51 +0100

    Added declaration for groupname_to_id

--- headers/UtilityFunctions.h
+++ headers/UtilityFunctions.h
@@ -3,8 +3,10 @@
 
 #include <string>
 #include <pwd.h>
+#include <grp.h>
 
 const std::string current_working_directory();
 uid_t username_to_id(const std::string &username);
+gid_t groupname_to_id(const std::string &groupname);
 
 #endif
