c49e9e3e583a6f7845f06b24d5afbe5bac6b8864
tagoras <pvaitkeviciuz@gmail.com>
2023-07-15 12:00:55 +0100

    Implementation of Chown::executeCommand

---
+++ Cpps/Chown.cpp
@@ -0,0 +1,8 @@
+#include "../headers/Chown.h"
+#include <unistd.h> // for chown()
+#include "../headers/UtilityFunctions.h"
+
+int Chown::executeCommand(const std::vector<std::string> &args) const 
+{
+    return chown(args[1].c_str(), username_to_id(args[0]), -1);
+}
