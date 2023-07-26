2c6110dd3fca5d4eeb527309610d496b289743b0
tagoras <pvaitkeviciuz@gmail.com>
2023-07-26 10:12:07 +0100

    Added signal handler object

--- headers/ApplicationRunner.h
+++ headers/ApplicationRunner.h
@@ -3,12 +3,14 @@
 
 #include "CommandParser.h"
 #include "CommandPicker.h"
+#include "SignalHandler.h"
 
 class Application_Runner{
     private:
-        CommandParser command_parser; 
-        CommandPicker command_picker;
-        ErrorManager error_manager;
+        CommandParser command_parser{}; 
+        CommandPicker command_picker{};
+        ErrorManager error_manager{};
+        SignalHandler signal_handler{};
     public:
         Application_Runner() = default;
         void run() const;
